b052f28521b8fa0cc00f2aa7c849da0a281049d5
nuancedtire <149303031+nuancedtire@users.noreply.github.com>
2024-08-30 02:09:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def create_post(timestamp, llm_timestamp, image_url, content, link):
     col1, col2 = st.columns([1, 5])
     
     with col1:
-        st.image(image_url, width=100)
+        st.image(image_url, width=200)
     
     with col2:
         st.write(f"**Publish Time:** {timestamp}")
