3ef13575a81c567282bcd55078a7d779a9576f78
nuancedtire <149303031+nuancedtire@users.noreply.github.com>
2024-08-30 02:10:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def create_post(timestamp, llm_timestamp, image_url, content, link):
         image_url = fallback_image_url
     
     # Create two columns for the thumbnail and the published time
-    col1, col2 = st.columns([1, 5])
+    col1, col2 = st.columns([2, 5])
     
     with col1:
         st.image(image_url, width=200)
