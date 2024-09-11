a54d263df68d79c10523b47d8e4ae4a999ea55f2
nuancedtire <149303031+nuancedtire@users.noreply.github.com>
2024-09-11 11:12:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -203,10 +203,7 @@ with st.sidebar:
 # Sidebar description
 st.sidebar.markdown("""Hello Team Peerr!
 
-This app is a demo frontend for displaying a feed of posts as they get updated. 
-The main section shows the latest posts, with each post displaying the publish time, 
-an image (open source), and a snippet of the content.
-You can expand each post to view the full content. You can also filter by date or tags below.
+This app is a demo frontend for displaying a feed of posts as they get updated.
 """)
 
 # Apply cleaning function to 'Hashtags' column
